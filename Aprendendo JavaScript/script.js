bd1c6d1cb7d0a5a5ecaa05caa43170a9b5d6fbc0
pedrouila <pedrouilac.sousa@gmail.com>
2023-05-07 00:19:20 -0300

    Adicionando aulas de miniprojetos em javascript

---
+++ Aprendendo JavaScript/script.js
@@ -0,0 +1,12 @@
+/*
+// ---- script aula 01 ----
+alert('Olá! Seja bem-vindo novamente!')
+*/
+
+/*
+// ----- script aula 02 ------
+alert('Olá! Seja bem-vindo novamente!')
+document.getElementById("nome").value = 'oi'
+*/
+
+
